044f2bf6862fd4f6c71466eeaaa56e78bff2579e
Gama, Antonino C <antonino.c.gama@accenture.com>
2019-10-12 14:23:31 -0300

    [AC]Profile mock

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -10,17 +10,55 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 })
 export class ProfilePage {
   @ViewChild('slides') slides: Slides;
-  perfil: any ={};
+  profiles: { label: string; type:string; placeholder:string; value:string }[] = [
+    {
+      label:'E aí, qual o seu nome?',
+      type:'text',
+      placeholder:'Seu nome completo',
+      value:''
+    },
+    {
+      label:`Myllena, me diz agora tua data de nascimento.`,
+      type:'text',
+      placeholder:'Seu nome completo',
+      value:''
+    },
+    {
+      label:`Conta aí, Myllena, qual é a tua meta?`,
+      type:'text',
+      placeholder:'Seu nome completo',
+      value:''
+    },
+    {
+      label:`Massa, vamos te ajudar nisso! Qual é o valor que queres alcançar?`,
+      type:'text',
+      placeholder:'digite aqui..',
+      value:''
+    },
+    {
+      label:`Já ta acabando tá?! Qual teu e-mail?`,
+      type:'text',
+      placeholder:'e-mail',
+      value:''
+    },
+    {
+      label:`Pra finalizar, a senha super secreta`,
+      type:'password',
+      placeholder:'Digite sua senha',
+      value:''
+    },
+  ];
   public event = {
     month: '',
   }
+
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
-    this.perfil = this.navParams.get('perfil');
   }
 
   nextSlide() {
