67a0c5218df9a665a506cd3888dd62323051944e
Thiago Queiroz <thiago.queiroz@accenture.com>
2019-10-12 15:57:32 -0300

    Ajust

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -10,49 +10,44 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 })
 export class ProfilePage {
   @ViewChild('slides') slides: Slides;
-  profiles: { label: string; type:string; placeholder:string; value:string }[] = [
+  profiles: { label: string; type: string; placeholder: string; value: string }[] = [
     {
-      label:'E aí, qual o seu nome?',
-      type:'text',
-      placeholder:'Seu nome completo',
-      value:''
+      label: 'E aí, qual o seu nome?',
+      type: 'text',
+      placeholder: 'Seu nome completo',
+      value: ''
     },
     {
-      label:`Myllena, me diz agora tua data de nascimento.`,
-      type:'text',
-      placeholder:'Seu nome completo',
-      value:''
+      label: `Myllena, me diz agora tua data de nascimento.`,
+      type: 'month',
+      placeholder: 'DD/MM/AAAA',
+      value: ''
     },
     {
-      label:`Conta aí, Myllena, qual é a tua meta?`,
-      type:'text',
-      placeholder:'Digite aqui..',
-      value:''
+      label: `Conta aí, Myllena, qual é a tua meta?`,
+      type: 'text',
+      placeholder: 'Digite aqui..',
+      value: ''
     },
     {
-      label:`Massa, vamos te ajudar nisso! Qual é o valor que queres alcançar?`,
-      type:'text',
-      placeholder:'digite aqui..',
-      value:''
+      label: `Massa, vamos te ajudar nisso! Qual é o valor que queres alcançar?`,
+      type: 'text',
+      placeholder: 'Digite aqui..',
+      value: ''
     },
     {
-      label:`Já ta acabando tá?! Qual teu e-mail?`,
-      type:'text',
-      placeholder:'e-mail',
-      value:''
+      label: `Já ta acabando tá?! Qual teu e-mail?`,
+      type: 'text',
+      placeholder: 'Digite seu e-mail',
+      value: ''
     },
     {
-      label:`Pra finalizar, a senha super secreta`,
-      type:'password',
-      placeholder:'Digite sua senha',
-      value:''
+      label: `Pra finalizar, a senha super secreta`,
+      type: 'password',
+      placeholder: 'Digite sua senha',
+      value: ''
     },
   ];
-  public event = {
-    month: '',
-  }
-
-
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -63,8 +58,9 @@ export class ProfilePage {
 
   nextSlide() {
     this.slides.slideNext();
-    if (this.slides._activeIndex == 5)
+    if (this.slides._activeIndex == 5) {
       this.navCtrl.setRoot('HomePage');
+    }
   }
 
   prevSlide() {
