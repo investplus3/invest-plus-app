a2713392124777a5c1bfeccbb81bd4c7348ead61
Gama, Antonino C <antonino.c.gama@accenture.com>
2019-10-12 04:07:24 -0300

    [AC] firebase login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDBLWJ7TeQ3SshlQbfZ0br_DKUh2lsYdks",
+  authDomain: "investplus-38006.firebaseapp.com",
+  databaseURL: "https://investplus-38006.firebaseio.com",
+  storageBucket: "investplus-38006.appspot.com",
+  measurementId: "G-Q6G19K3B94"
+};
 
 import { MyApp } from './app.component';
 
@@ -12,7 +22,9 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @IonicPage()
 @Component({
@@ -7,11 +8,52 @@ import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angu
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  username:any;
+  password:any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public toastCtrl: ToastController) {
+    public toastCtrl: ToastController,
+    public firebaseauth: AngularFireAuth)  {
+
+  }
+
+
+  public LoginComEmail(): void {
+    this.firebaseauth.auth.signInWithEmailAndPassword(this.username.value , this.password.value)
+      .then(() => {
+        this.exibirToast('Login efetuado com sucesso');
+      })
+      .catch((erro: any) => {
+        this.exibirToast(erro);
+      });
+  }
+
+  public cadastrarUsuario(): void {
+    this.firebaseauth.auth.createUserWithEmailAndPassword(this.username.value , this.password.value)
+    .then(() => {
+      this.exibirToast('Usuário criado com sucesso');
+    })
+    .catch((erro: any) => {
+      this.exibirToast(erro);
+    });
+  }
+
+  public Sair(): void {
+    this.firebaseauth.auth.signOut()
+    .then(() => {
+      this.exibirToast('Você saiu');
+    })
+    .catch((erro: any) => {
+      this.exibirToast(erro);
+    });
+  }
+
+  private exibirToast(mensagem: string): void {
+    let toast = this.toastCtrl.create({duration: 3000,
+                                      position: 'botton'});
+    toast.setMessage(mensagem);
+    toast.present();
   }
 
   account: { username: string, password: string } = {
